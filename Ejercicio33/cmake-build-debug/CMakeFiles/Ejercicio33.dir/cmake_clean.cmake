6b1bdee246107d939b9d6ce9519ed3ac341d3c3d
Mathias Salva <mattsalva@outlook.com>
2021-06-24 16:41:47 -0300

    Ejercio 33 listoo

---
+++ Ejercicio33/cmake-build-debug/CMakeFiles/Ejercicio33.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio33.dir/main.cpp.obj"
+  "Ejercicio33.exe"
+  "Ejercicio33.exe.manifest"
+  "Ejercicio33.pdb"
+  "libEjercicio33.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio33.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
