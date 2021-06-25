99322246dabb73b56b27017a22e1b91c9e423d61
MattSalva <mattsalva@outlook.com>
2021-06-25 19:44:25 -0300

    E39 y fix espacios nombres de carpetas

---
+++ Ejercicio_39/cmake-build-debug/CMakeFiles/Ejercicio39.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio39.dir/main.cpp.obj"
+  "Ejercicio39.exe"
+  "Ejercicio39.exe.manifest"
+  "Ejercicio39.pdb"
+  "libEjercicio39.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio39.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
