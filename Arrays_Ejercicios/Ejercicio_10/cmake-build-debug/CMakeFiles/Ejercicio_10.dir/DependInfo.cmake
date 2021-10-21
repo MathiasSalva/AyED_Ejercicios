9dc12765c19b11f19b388eebde5059f0532a90c8
MattSalva <mattsalva@outlook.com>
2021-10-21 17:48:38 -0300

    Terminado, agregado burbujeo

--- Arrays_Ejercicios/Ejercicio_10/cmake-build-debug/CMakeFiles/Ejercicio_10.dir/DependInfo.cmake
+++ Arrays_Ejercicios/Ejercicio_10/cmake-build-debug/CMakeFiles/Ejercicio_10.dir/DependInfo.cmake
@@ -1,15 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/mathias.salva/CLionProjects/AyED_Ejercicios/Arrays_Ejercicios/Ejercicio_10/main.cpp" "C:/Users/mathias.salva/CLionProjects/AyED_Ejercicios/Arrays_Ejercicios/Ejercicio_10/cmake-build-debug/CMakeFiles/Ejercicio_10.dir/main.cpp.obj"
+  "C:/Users/matts/RepoUTN/AyED_Ejercicios/Arrays_Ejercicios/Ejercicio_10/main.cpp" "C:/Users/matts/RepoUTN/AyED_Ejercicios/Arrays_Ejercicios/Ejercicio_10/cmake-build-debug/CMakeFiles/Ejercicio_10.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
--- Arrays_Ejercicios/Ejercicio_10/main.cpp
+++ Arrays_Ejercicios/Ejercicio_10/main.cpp
@@ -1,56 +1,73 @@
 #include <iostream>
-#include <cstdio>
+#include <string.h>
+#include <cstdio> //fflush
 #include <cstdlib>
 #include <cstring>
-#include <conio.h>
+#include <conio.h> //getch
+#include <stdlib.h> //atoi
 
 
 using namespace std;
 
-void initTab(char t[][5][50]){
-    for (int i = 0; i < 50; i++){
-        for (int j = 0; j < 5; j++){
-            strcpy(t[3][50], "");
-        }
-    }
-}
-
-void ingresarDatos(char t[][5][50], int fila){
-    cout << "Ingrese apellido y nombre" << endl;
-    fflush(stdin);
-    gets(t[fila][0]);
-    cout << "Ingrese Direccion" << endl;
-    fflush(stdin);
-    gets(t[fila][1]);
-    cout << "Ingrese Edad" << endl;
-    cin >> t[fila][2];
-    cout << "Ingrese Saldos" << endl;
-    cin >> t[fila][3];
-
-}
-
-void burbujeo(){
-
-
-
-}
-
-void mostrarTabla(char t[][5][50], int filas){
+void initTable(char t[][4][50], int filas){
     for (int i = 0; i < filas; i++){
-        cout << "| " << t[i][0] << " | " << t[i][1] << " |";
-        cout << "| " << t[i][2] << " | " << t[i][3] << " |" << endl;
+        for (int j = 0; j < 4; j++){
+
+            strcpy(t[i][j], "");
+
+        }
     }
     return;
 }
 
-int main() {
-    char tabla[10][5][50];
-    ingresarDatos(tabla,0);
-    ingresarDatos(tabla,1);
-    ingresarDatos(tabla,2);
-    ingresarDatos(tabla,3);
+void ingresarDatos(char t[][4][50], int fila){
+    cout << "Ingresar apellido y nombre" << endl;
+    fflush(stdin);
+    gets(t[fila][0]);
+    cout << "Ingrese direccion" << endl;
+    fflush(stdin);
+    gets(t[fila][1]);
+    cout << "Ingrese edad" << endl;
+    cin >> t[fila][2];
+    cout << "Ingrese saldos" << endl;
+    cin >> t[fila][3];
 
-    mostrarTabla(tabla, 3);
+    return;
 
+}
+
+void mostrarDatos(char t[][4][50], int filas){
+    cout << "| Apellido y Nombre | Direccion | Edad | Salario |" <<endl;
+    for (int i = 0; i < filas; i++){
+        cout << "| " << t[i][0] << " | " << t[i][1] << " | " << t[i][2] << " | " << t[i][3] << " |" << endl;
+        }
+    }
+void burbuja(char t[][4][50], int filas){
+    char aux[50];
+    for (int i = 0; i < filas - 1; i++){
+        for (int j = 0; j < filas - 1 - i; j++){
+            if (atoi(t[j+1][3]) < atoi(t[j][3])){
+
+                for (int col = 0; col < 4; col++){
+                    strcpy(aux, t[j][col]);
+                    strcpy(t[j][col], t[i+1][col]);
+                    strcpy(t[j+1][col],aux);
+                }
+
+            }
+
+        }
+    }
+}
+
+
+int main(){
+    char lista[4][4][50];
+    initTable(lista, 4);
+    ingresarDatos(lista, 0);
+    ingresarDatos(lista, 1);
+    mostrarDatos(lista, 2);
+    burbuja(lista, 4);
+    mostrarDatos(lista, 2);
     return 0;
 }
