71a26b413ded8411d23d887102d3bda048031b6d
MattSalva <mattsalva@outlook.com>
2021-10-23 17:38:14 -0300

    Pilas 13 sin completar

---
+++ Arrays_Ejercicios/Ejercicio_13_Pilas/cmake-build-debug/CMakeFiles/Ejercicio_13_Pilas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Ejercicio_13_Pilas.
+# This may be replaced when dependencies are built.
---
+++ Arrays_Ejercicios/Ejercicio_13_Pilas/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void push(int vec[], int tam){
+
+}
+
+int main() {
+
+    int vec[20];
+
+
+
+    return 0;
+}
