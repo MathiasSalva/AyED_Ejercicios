4dfc73452891e22ed866d190ac14a48827bced9d
Mathias Salva <mattsalva@outlook.com>
2021-10-23 20:53:11 -0300

    queue, mostrar, inicializar

--- Arrays_Ejercicios/Ejercicio_13_Pilas/main.cpp
+++ Arrays_Ejercicios/Ejercicio_13_Pilas/main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
+#define VACIO -999999
+using namespace std;
 
-void push(int vec[], int tam){
+void inicializarVacio(int vec[], int tam){
+    for (int i = 0; i < tam; i++){
+        vec[i] = VACIO;
+    }
+}
 
+bool queue(int vec[], int tam, int el){
+    int i = 0;
+    while (i < tam && vec[i] != VACIO)
+        i++;
+    if (i >= tam){
+        cout << "Vector lleno" << endl;
+        return false;
+    }
+    else{
+        vec[i] = el;
+        return true;
+    }
+}
+
+bool unqueue(int vec[], int tam){
+    //TODO Funcion para sacar primer elemento de la fila y mover el resto una posicion menos
+}
+
+void mostrarVector(int vec[], int tam){
+    for (int i = 0; i < tam && vec[i] != VACIO; i++)
+        cout << vec[i] << " ";
 }
 
 int main() {
 
     int vec[20];
+    inicializarVacio(vec, 20);
 
+    queue(vec, 20, 1);
+    queue(vec, 20, 2);
+    queue(vec, 20, 3);
+    queue(vec, 20, 4);
+    queue(vec, 20, 5);
+
+    mostrarVector(vec, 20);
 
 
     return 0;
